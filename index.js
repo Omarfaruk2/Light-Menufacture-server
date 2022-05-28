32dbbcaf5c9f7f8d6a4d785749284ba8b61c0c54
Omarfaruk4200 <emon35293@gmail.com>
2022-05-29 05:44:03 +0600

    added order list

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0wfi.mongodb.net/?retryWrites=true&w=majority`
@@ -20,7 +22,8 @@ async function run() {
         await client.connect()
         const toolsCollection = client.db("tools").collection("light")
         const reviewsCollection = client.db("tools").collection("reviews")
-        // const userCollection = client.db("tools").collection("users")
+        const OrderCollection = client.db("tools").collection("order")
+        const userCollection = client.db("tools").collection("users")
 
 
 
@@ -39,18 +42,22 @@ async function run() {
             res.send(result)
         })
 
-        // app.put("/tool/:id", async (req, res) => {
-        //     const id = req.params.id
-        //     const info = req.body
-        //     const filter = { id: id }
+        app.post("/order", async (req, res) => {
+            // const id = req.params.id
+            const info = req.body
+            const result = await OrderCollection.insertOne(info)
+            res.send(result)
+        })
 
-        //     const updateDoc = {
-        //         $set: info,
-        //     }
-        //     const result = await userCollection.updateOne(filter, updateDoc, options)
-        //     // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
-        //     res.send(result)
-        // })
+        app.get('/order/:email', async (req, res) => {
+
+            const email = req.params.email
+            const filter = { email: email }
+            const cursor = OrderCollection.find(filter)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
 
         app.get('/review', async (req, res) => {
             const query = {}
@@ -65,7 +72,32 @@ async function run() {
             res.send(result)
         })
 
+
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            console.log(user, email)
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, options, updateDoc)
+            res.send(result)
+        })
+
+        app.get("/user/:email", async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await userCollection.findOne(filter)
+            res.send(result)
+        })
+
+
+
+
     }
+
     finally {
 
     }
