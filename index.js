83c2ec053e22bc77ef050cfb292e6e5dc4444d50
Omarfaruk4200 <emon35293@gmail.com>
2022-05-29 21:51:15 +0600

    added delect items

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
-
+const jwt = require('jsonwebtoken')
 app.use(cors())
 app.use(express.json())
 
@@ -14,7 +14,23 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0wfi.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
+async function verifyJWT(req, res, next) {
+    const authheader = req.headers.authorization
+    if (!authheader) {
+        return res.status(401).send({ message: "Unauthorize access" })
+    }
+    const token = authheader.split(" ")[1]
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" })
+        }
+        req.decoded = decoded
+        next()
+
+    })
+
+}
 
 
 async function run() {
@@ -25,7 +41,40 @@ async function run() {
         const OrderCollection = client.db("tools").collection("order")
         const userCollection = client.db("tools").collection("users")
 
+        const newuserCollection = client.db("tools").collection("newusers")
 
+
+
+        app.put("/newusers/:email", async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user
+            }
+            const result = await newuserCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
+
+        app.put("/newusers/admin/:email", verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: "admin" }
+            }
+            const result = await newuserCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+        app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email
+            const user = await newuserCollection.findOne({ email: email })
+            const isAdmin = user.role === "admin"
+            res.send({ admin: isAdmin })
+        })
 
         app.get('/tool', async (req, res) => {
             const query = {}
@@ -56,7 +105,31 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/order/:email', async (req, res) => {
+
+        // new
+        app.get("/order", async (req, res) => {
+            const email = req.query.email
+            const decodedEmail = req.decoded.email
+            if (email === decodedEmail) {
+
+                const query = { email: email }
+                const order = await OrderCollection.find(query).toArray()
+                return res.send(order)
+            }
+            else {
+                return res.status(403).send({ message: "Forbidden access" })
+            }
+        })
+        // new end
+
+        app.get("/newusers", verifyJWT, async (req, res) => {
+            const users = await newuserCollection.find().toArray()
+            res.send(users)
+        })
+
+
+
+        app.get('/order/:email', verifyJWT, async (req, res) => {
 
             const email = req.params.email
             const filter = { email: email }
@@ -65,16 +138,17 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/order/:email', async (req, res) => {
+        app.delete('/order/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await OrderCollection.deleteOne(query)
             res.send(result)
         })
 
+
         // deleted itemms
         app.delete('/tool/:id', async (req, res) => {
-            const id = req.body.id
+            const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await toolsCollection.deleteOne(query)
             res.send(result)
@@ -87,6 +161,7 @@ async function run() {
             res.send(result)
         })
 
+
         app.post('/review', async (req, res) => {
             const review = req.body
             const result = await reviewsCollection.insertOne(review)
@@ -94,16 +169,16 @@ async function run() {
         })
 
 
+
         app.put("/user/:email", async (req, res) => {
             const email = req.params.email
             const user = req.body
-            console.log(user, email)
             const filter = { email: email }
             const options = { upsert: true }
             const updateDoc = {
                 $set: user
             }
-            const result = await userCollection.updateOne(filter, options, updateDoc)
+            const result = await userCollection.updateOne(filter, updateDoc, options)
             res.send(result)
         })
 
