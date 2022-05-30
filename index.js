34f793d50f6effc13844e72103272054e5138532
Omarfaruk4200 <emon35293@gmail.com>
2022-05-30 19:51:48 +0600

    added delecte order

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
-
+        // delecte order
         app.delete('/order/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
